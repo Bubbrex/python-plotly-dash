ac4a49a68f26d0c21ecf6dbd15b4894900111a5e
Yichao Ma <61528973+Bubbrex@users.noreply.github.com>
2022-03-25 01:19:39 +0000

    update

--- test2.py
+++ test2.py
@@ -10,9 +10,9 @@ app = Dash(__name__)
 # Import Data
 
 df = pd.read_excel("./data/footprint_criteria.xlsx", index_col=0,
-                   dtype={"coef_val": "str", "coef_swiped": "str", "fz": "int32", "camber": "int32"})
+                   dtype={"coef_val": "str", "coef_swiped": "str", "fz": "int32", "camber": "int32","coef_ratio":"str"})
 
-
+df["coef_ratio"] = df["coef_ratio"].astype("float")
 # App Layout
 
 options_coef_swiped = [
@@ -126,13 +126,13 @@ app.layout = html.Div([
             dcc.Graph(id="my_graph", figure={} ,className = "graph"),
             html.Div(id="slider_info", style={"margin":"1rem"},children=[]),
             html.Div(id="slider",children=[
-                dcc.Slider(0.5, 1.9, 0.1,id="coef_ratio", value=1.2)]),
+                dcc.Slider(0.5, 1.9, step=None,id="coef_ratio", marks={},value=1.2,)]),
             html.Div(id="slider2",children=[
                 html.Div(id="slider_info2", style={"margin":"1rem"},children=[]),
-                dcc.Slider(0.5, 1.9, 0.1,id="coef_ratio2", value=0.7)]),
+                dcc.Slider(0.5, 1.9, 0.1,id="coef_ratio2", value=0.7,marks={})]),
             html.Div(id="slider3",children=[
                 html.Div(id="slider_info3", style={"margin":"1rem"},children=[]),
-                dcc.Slider(0.5, 1.9, 0.1, id="coef_ratio3", value=1.5)]),
+                dcc.Slider(0.5, 1.9, 0.1, id="coef_ratio3", value=1.5,marks={})]),
             ]),
 ])
 
@@ -147,7 +147,10 @@ app.layout = html.Div([
     Output(component_id='dropdown2', component_property='style'),
     Output(component_id='dropdown3', component_property='style'),
     Output(component_id='slider2', component_property='style'),
-    Output(component_id='slider3', component_property='style'),],     
+    Output(component_id='slider3', component_property='style'),
+    Output(component_id='coef_ratio', component_property='marks'),
+    Output(component_id='coef_ratio2', component_property='marks'),
+    Output(component_id='coef_ratio3', component_property='marks'),],     
     [
     Input(component_id='coef_swiped', component_property='value'),
     Input(component_id='fz', component_property='value'),
@@ -165,7 +168,7 @@ app.layout = html.Div([
 )
 def update_graph(coef_swiped, fz, camber, coef_ratio,coef_swiped2, fz2, camber2, coef_ratio2,coef_swiped3, fz3, camber3, coef_ratio3,radio):
     
-    print(coef_ratio,coef_ratio2,coef_ratio3)
+    # print(coef_ratio,coef_ratio2,coef_ratio3)
 
     dropdown2= {"display":"none"}
     slider2 = {"display":"none"}
@@ -174,6 +177,12 @@ def update_graph(coef_swiped, fz, camber, coef_ratio,coef_swiped2, fz2, camber2,
     coef_val = 0
     coef_val2 = 0
     coef_val3 = 0
+    slider_info = ""
+    slider_info2 = ""
+    slider_info3 = ""
+    marks1 = {}
+    marks2 = {}
+    marks3 = {}
 
     if radio >=2:
         
@@ -189,49 +198,69 @@ def update_graph(coef_swiped, fz, camber, coef_ratio,coef_swiped2, fz2, camber2,
         dff = dff[dff["coef_swiped"] == coef_swiped]
         dff = dff[dff["fz"] == fz]
         dff = dff[dff["camber"] == camber]
+
+        cf_unique = sorted(dff["coef_ratio"].unique())
+        marks = dict(zip(cf_unique,[str(x) for x in cf_unique]))
+
         dff = dff[dff["coef_ratio"] == coef_ratio]
-        coef_val = dff.iloc[0]["coef_val"]
-        file = dff.iloc[0]["file"]
-        current_path = "./data/" + coef_swiped + "/" + \
-            str(coef_val).replace(
-            ".", "_") + "/"+str(file)
-        dfx = pd.read_fwf(current_path)
-        return dfx, coef_val
+        if(dff.empty):
+            coef_val = -1
+            return dff,coef_val
+        else:
+            coef_val = dff.iloc[0]["coef_val"]
+            file = dff.iloc[0]["file"]
+            current_path = "./data/" + coef_swiped + "/" + \
+                str(coef_val).replace(
+                ".", "_") + "/"+str(file)
+            dfx = pd.read_fwf(current_path)
+            return dfx, coef_val,marks
 
 
     fig = go.Figure()
 
-    dfx,coef_val = getXY(coef_swiped,fz,camber,coef_ratio)
-    fig.add_trace(go.Scatter(
-        visible=True,
-        x=dfx.x_ftire,
-        y=dfx.y_ftire,
-        line=go.scatter.Line(color="rgba(93,172,129,0.8)",width=5),
-        name="First Trace"
-        ))
-    if radio >= 2:
-        dfx2,coef_val2 = getXY(coef_swiped2,fz2,camber2,coef_ratio2)
+    dfx,coef_val,marks1 = getXY(coef_swiped,fz,camber,coef_ratio)
+    if(coef_val==-1):
+        slider_info = "N/A"
+    else:
+        slider_info = "The coefficient value is {}".format(coef_val)  
         fig.add_trace(go.Scatter(
             visible=True,
-            x=dfx2.x_ftire,
-            y=dfx2.y_ftire,
-            line=go.scatter.Line(color="gray",width=5),
-            name="Second Trace"
-        ))
- 
-        if radio == 3:
-            dfx3,coef_val3 = getXY(coef_swiped3,fz2,camber3,coef_ratio3)
+            x=dfx.x_ftire,
+            y=dfx.y_ftire,
+            line=go.scatter.Line(color="rgba(93,172,129,0.8)",width=5),
+            name="First Trace"
+            ))
+    if radio >= 2:
+        dfx2,coef_val2,marks2 = getXY(coef_swiped2,fz2,camber2,coef_ratio2)
+        print(marks2)
+
+        if(coef_val2==-1):
+            slider_info2 = "N/A"
+        else:
+            slider_info2 = "The coefficient value is {}".format(coef_val2)    
             fig.add_trace(go.Scatter(
                 visible=True,
-                x=dfx3.x_ftire,
-                y=dfx3.y_ftire,
-                line=go.scatter.Line(color="blue",width=5),
-                 name="Third Trace"
+                x=dfx2.x_ftire,
+                y=dfx2.y_ftire,
+                line=go.scatter.Line(color="gray",width=5),
+                name="Second Trace"
             ))
-            
-    slider_info = "The coefficient value is {}".format(coef_val)  
-    slider_info2 = "The coefficient value is {}".format(coef_val2)    
-    slider_info3 = "The coefficient value is {}".format(coef_val3)    
+    
+        if radio == 3:
+            dfx3,coef_val3,marks3 = getXY(coef_swiped3,fz2,camber3,coef_ratio3)
+            if(coef_val3 == -1):
+                slider_info3 = "N/A"
+            else:
+                slider_info3 = "The coefficient value is {}".format(coef_val3)    
+                fig.add_trace(go.Scatter(
+                    visible=True,
+                    x=dfx3.x_ftire,
+                    y=dfx3.y_ftire,
+                    line=go.scatter.Line(color="blue",width=5),
+                    name="Third Trace"
+                ))
+                
+    
 
     
 
@@ -242,7 +271,7 @@ def update_graph(coef_swiped, fz, camber, coef_ratio,coef_swiped2, fz2, camber2,
         title_x=0.5
     )
 
-    return slider_info,slider_info2,slider_info3, fig,dropdown2,dropdown3,slider2,slider3
+    return slider_info,slider_info2,slider_info3, fig,dropdown2,dropdown3,slider2,slider3,marks1,marks2,marks3
 
 
 # Run the dash
